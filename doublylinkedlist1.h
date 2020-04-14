ccf4d9f8a67f4fcc52357f0d7ad0f4d344ac7a66
Yuki Chen <yingchen@chapman.edu>
2020-04-13 17:55:24 -0700

    .h file

---
+++ doublylinkedlist1.h
@@ -0,0 +1,13 @@
+class  doublylinkedlist1{
+  public:
+    int data;
+    //pointer to linkedlist
+    doublylinkedlist1 *next;
+    doublylinkedlist1 *prev;
+
+
+    doublylinkedlist1();//default constructor
+    doublylinkedlist1(int d);//overloaded constructor
+    ~doublylinkedlist1();//destructor
+
+};
