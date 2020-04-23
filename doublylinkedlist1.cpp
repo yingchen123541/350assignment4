fbc28540ae775eb928d4e4dd91deeca89af0f989
Yuki Chen <yingchen@chapman.edu>
2020-04-22 17:18:47 -0700

    comment

--- doublylinkedlist1.cpp
+++ doublylinkedlist1.cpp
@@ -1,23 +1,37 @@
-doublylinkedlist::doublylinkedlist(){
+#include "doublylinkedlist1.h"
+
+/** doublylinkedlist1.cpp
+* Name: Yuki Chen
+* Student ID: 2320235
+* Email: yingchen@chapman.edu
+*
+* Assignment4  Registrar’s Office Simulation
+* purpose: implement listnode to use in doubly1.h and doubly1.cpp 
+* Version 3.0
+* Date: April 22, 2020
+*
+*/
+
+doublylinkedlist1::doublylinkedlist1(){
   data=0;
   //pointer always set to null, cause when remove and add things, not sure where it's pointing to
   next=NULL;
   prev=NULL;
 }
 //overload constructor
-doublylinkedlist::doublylinkedlist(int d){
+doublylinkedlist1::doublylinkedlist1(int d){
   data=d;
   next=NULL;
   prev=NULL;
 }
 //destructor
-doublylinkedlist::~doublylinkedlist(){
+doublylinkedlist1::~doublylinkedlist1(){
 
 }
 
 //need to find the value in the list before we can delete
 //this function does not take in a position as a parameter
-int doublylinkedlist::remove(int val){
+int doublylinkedlist1::remove(int val){
   doubly *curr = front;
   while(curr->data != val)
   {
@@ -48,12 +62,12 @@ int doublylinkedlist::remove(int val){
   return temp;
 }
 
-int doublylinkedlist::search(int value){
+int doublylinkedlist1::search(int value){
 //almost the same with single linked list, don't need to worry about backward pointers, cause search then go to the next
 //variable to keep track of position to loop through whole list and search
 //if position is negative, that value is not found, default at -1
 int position = -1;
-ListNode *curr = front;
+doublylinkedlist1 *curr = front;
 while (curr != NULL){
   //iterate and attempt to find the value
   position++;
